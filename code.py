65d4e9d2c10efc8bf99fd7ec5f10c11dd68f6ac9
Jari <safijari@users.noreply.github.com>
2024-07-09 00:54:29 +0000

    Create code.py

---
+++ code.py
@@ -0,0 +1,99 @@
+import time
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.mouse import Mouse
+from adafruit_hid.keycode import Keycode
+import ipaddress
+import json
+
+import wifi
+wifi_creds = json.load(open("wifi.json"))
+wifi.radio.connect(wifi_creds["ssid"], wifi_creds["password"])
+
+from asyncio import create_task, gather, run, sleep as async_sleep
+import board
+import microcontroller
+import neopixel
+import socketpool
+import wifi
+
+from adafruit_httpserver import Server, Request, Response, Websocket, GET
+
+keyboard = Keyboard(usb_hid.devices)
+mouse = Mouse(usb_hid.devices)
+pool = socketpool.SocketPool(wifi.radio)
+server = Server(pool, debug=True)
+
+pixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
+
+websocket: Websocket = None
+
+HTML_TEMPLATE = open("index.html").read()
+
+
+@server.route("/client", GET)
+def client(request: Request):
+    return Response(request, HTML_TEMPLATE, content_type="text/html")
+
+
+@server.route("/connect-websocket", GET)
+def connect_client(request: Request):
+    global websocket  # pylint: disable=global-statement
+
+    if websocket is not None:
+        websocket.close()  # Close any existing connection
+
+    websocket = Websocket(request)
+
+    return websocket
+
+
+server.start(str(wifi.radio.ipv4_address))
+
+
+async def handle_http_requests():
+    while True:
+        server.poll()
+        await async_sleep(0)
+
+async def run_ping():
+    while True:
+        print(wifi.radio.ping(ipaddress.IPv4Address("8.8.8.8")))
+        print(wifi.radio.hostname, wifi.radio.ipv4_gateway, wifi.radio.ipv4_address)
+        await async_sleep(1)
+
+async def handle_websocket_requests():
+    while True:
+        if websocket is not None:
+            if (data := websocket.receive(fail_silently=True)) is not None:
+                val = json.loads(data)
+                print(val)
+                if val["type"] == "mousemove":
+                    mouse.move(int(val["dx"]), int(val["dy"]))
+                # print(data)
+                # key = data[0]
+                # keyboard.press(Keycode.A)  # Change this to the appropriate Keycode
+                # keyboard.release_all()
+                # mouse.move(50, 50)
+        await async_sleep(0)
+
+
+async def send_websocket_messages():
+    while True:
+        if websocket is not None:
+            cpu_temp = round(microcontroller.cpu.temperature, 2)
+            websocket.send_message(str(cpu_temp), fail_silently=True)
+
+        await async_sleep(1)
+
+
+async def main():
+    await gather(
+        create_task(run_ping()),
+        create_task(handle_http_requests()),
+        create_task(handle_websocket_requests()),
+        create_task(send_websocket_messages()),
+    )
+
+
+run(main())
